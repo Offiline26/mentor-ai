df53e7c5ce7bfad9582c3c7a61cd4c817f4f14c0
Thiago Mendes do Nascimento <124217018+Offiline26@users.noreply.github.com>
2025-11-11 22:23:12 -0300

    Iniciando projeto da GS - FIAP

---
+++ src/main/java/br/com/fiap/mentorai/MentorAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mentorai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/mentorai/MentorAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mentorai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
