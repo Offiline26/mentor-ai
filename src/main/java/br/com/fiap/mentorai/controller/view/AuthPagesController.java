d721de50a923673c3016b8b3c5b9f92ee7c0ef53
offiline2606 <offline.2606@gmail.com>
2025-11-17 16:13:02 -0300

    registro e login funcionando

--- src/main/java/br/com/fiap/mentorai/controller/view/AuthPagesController.java
+++ src/main/java/br/com/fiap/mentorai/controller/view/AuthPagesController.java
@@ -1,18 +1,32 @@
 package br.com.fiap.mentorai.controller.view;
 
+import br.com.fiap.mentorai.repository.AreaAtuacaoRepository;
+import br.com.fiap.mentorai.repository.CargoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthPagesController {
 
+    private final CargoRepository cargoRepository;
+    private final AreaAtuacaoRepository areaAtuacaoRepository;
+
+    public AuthPagesController(CargoRepository cargoRepository,
+                               AreaAtuacaoRepository areaAtuacaoRepository) {
+        this.cargoRepository = cargoRepository;
+        this.areaAtuacaoRepository = areaAtuacaoRepository;
+    }
+
     @GetMapping("/login")
     public String loginPage() {
-        return "login";     // -> templates/login.html
+        return "login";     // templates/login.html
     }
 
     @GetMapping("/register")
-    public String registerPage() {
-        return "register";  // -> templates/register.html
+    public String registerPage(Model model) {
+        model.addAttribute("cargos", cargoRepository.findAll());
+        model.addAttribute("areasAtuacao", areaAtuacaoRepository.findAll());
+        return "register";  // templates/register.html
     }
 }
